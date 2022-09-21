c6ced2449f50638ceade6759f3458563439b341f
jorgeprodriguezg <48585065+jorgeprodriguezg@users.noreply.github.com>
2022-09-21 12:10:13 +0200

    Update mla.py

--- mla/mla.py
+++ mla/mla.py
@@ -5,8 +5,8 @@ from PIL import Image
 st.title('Multiple language abstracts')
 with st.sidebar:
     st.subheader('Jorge P. Rodríguez, PhD') 
-#     image = Image.open('./resources/pic.jpeg')
-#     st.image(image,width=200)
+    image = Image.open('./pic.jpeg')
+    st.image(image,width=200)
     st.write('[Home](http://jorgeprodriguezg.github.io)')
     st.write('[Multiple language abstracts](https://jorgeprodriguezg-jorgeprodriguezg-github-io-mlamla-mq0osn.streamlitapp.com/)')
     st.write('[Data and code](http://jorgeprodriguezg.github.io/data_and_code.html)')
